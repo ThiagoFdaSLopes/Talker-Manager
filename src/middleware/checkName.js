e15600bf86dd7e7e0921f2c94c7e1fa67340cb2b
Thiago <programadorthiagolopes@gmail.com>
2022-12-14 15:56:24 -0300

    feat(name): Criado middleware checkName

---
+++ src/middleware/checkName.js
@@ -0,0 +1,18 @@
+const checkName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name && name.length === '') {
+      return res.status(400).json(
+      { message: 'O campo "name" é obrigatório' },
+    ); 
+  }
+  
+  if (name.length < 3) {
+    return res.status(400)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
+  }
+
+  next();
+};
+
+module.exports = checkName;
