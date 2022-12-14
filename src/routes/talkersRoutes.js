5db1f87c0e42ebac4475e9e9fb51877cf3abeb97
Thiago <programadorthiagolopes@gmail.com>
2022-12-14 15:10:40 -0300

    feat(req1): Criado rota para listar os Talkers]

--- src/routes/talkersRoutes.js
+++ src/routes/talkersRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const readFile = require('../utils/fs/readFile');
 
 const router = express.Router();
 
-router.get('/talker', (req, res) => {
-  res.status(200).json({ message: 'Estou aqui' });
+router.get('/talker', async (_req, res) => {
+  const db = await readFile();
+  res.status(200).json(db);
 });
 
 module.exports = router;
