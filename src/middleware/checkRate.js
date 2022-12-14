b29385601b878c36fa87a8e9a9c6987a368648c0
Thiago <programadorthiagolopes@gmail.com>
2022-12-14 18:26:52 -0300

    feat(req6): Refatorando o req6 para validar outros testes

--- src/middleware/checkRate.js
+++ src/middleware/checkRate.js
@@ -4,7 +4,6 @@ const checkRate = (req, res, next) => {
   if (!Number.isInteger(rate)) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
-  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   if (rate < 1 || rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
--- src/middleware/checkRateExists.js
+++ src/middleware/checkRateExists.js
@@ -1,8 +1,16 @@
 const checkRateExists = (req, res, next) => {
   const { talk } = req.body;
   const { rate } = talk;
-
+  
+  if (rate === 0) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  
   if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+
+  if (typeof rate !== 'number') {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' }); 
+  }
 
   next();
 };
