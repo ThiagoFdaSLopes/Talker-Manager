8397eabce028094004e6019a1c5078a61ee67d51
Thiago <programadorthiagolopes@gmail.com>
2022-12-14 16:09:01 -0300

    feat(talk): Criado middleware checkTalk

---
+++ src/middleware/checkTalk.js
@@ -0,0 +1,14 @@
+const checkTalk = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  if (!talk.watchedAt) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" é obrigatório' }); 
+  }
+  if (!talk.rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+
+  next();
+};
+
+module.exports = checkTalk;
