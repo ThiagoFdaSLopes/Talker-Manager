d5305b28e40bebb552773f2ebf4ad5d0dc898298
Thiago <programadorthiagolopes@gmail.com>
2022-12-14 16:12:01 -0300

    feat(checkWatchedAt): Criado middleware checkWatchedAt

---
+++ src/middleware/checkWatchedAt.js
@@ -0,0 +1,12 @@
+const checkWatchedAt = (req, res, next) => {
+  const { watchedAt } = req.body.talk;
+
+  const isFormatDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!isFormatDate.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+module.exports = checkWatchedAt;
